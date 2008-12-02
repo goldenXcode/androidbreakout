20cdec0521ee9bc076c834fc90de1ad15585aad7
clarkdawg <clarkdawg@921282de-c0be-11dd-bb00-bd6ab96fe29a>
2008-12-02 22:16:43 +0000

    Initial checkin of code.

---
+++ src/com/android/breakout/AndroidBreakout.java
@@ -0,0 +1,58 @@
+package com.android.breakout;
+
+import android.app.Activity;
+import android.hardware.SensorListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+
+public class AndroidBreakout extends Activity {
+    private GraphView mGraphView;
+    private SensorManager mSensorManager;
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        mGraphView = new GraphView(this);
+        setContentView(mGraphView);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(mGraphView, 
+                SensorManager.SENSOR_ACCELEROMETER,
+//                SensorManager.SENSOR_ORIENTATION | 
+                SensorManager.SENSOR_DELAY_FASTEST);
+    }
+    
+    @Override
+    protected void onStop() {
+        mSensorManager.unregisterListener(mGraphView);
+        super.onStop();
+    }
+/*
+    public void onSensorChanged(int sensor, float[] values) {
+        //Log.d(TAG, "sensor: " + sensor + ", x: " + values[0] + ", y: " + values[1] + ", z: " + values[2]);
+        synchronized (this) {
+            if (mBitmap != null) {
+                final Canvas canvas = mCanvas;
+                final Paint paint = mPaint;
+               	if (sensor == SensorManager.SENSOR_ACCELEROMETER) {
+               		mPaddleX += (int)(values[0] * 2.0f);
+               		if (mPaddleX < 0)
+               			mPaddleX = 0;
+               		if (mPaddleX > ((int)mWidth-mPaddleWidth))
+               			mPaddleX = (int)mWidth - mPaddleWidth;
+               	}
+                invalidate();
+            }
+        }
+    }
+*/
+    public void onAccuracyChanged(int sensor, int accuracy) {
+        // TODO Auto-generated method stub
+        
+    }
+}
---
+++ src/com/android/breakout/GameManager.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package com.android.breakout;
+
+/**
+ * @author lithium
+ *
+ */
+public class GameManager {
+
+}
